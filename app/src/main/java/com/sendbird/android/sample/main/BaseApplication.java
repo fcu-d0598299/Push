e7f5adb94ac9b9fb6e0b5dc9e32a67e3272fc757
fcu-d0449609 <a0928018329@gmail.com>
2018-10-30 22:18:49 +0800

    嘗試整合(失敗)

--- app/src/main/java/com/sendbird/android/sample/main/BaseApplication.java
+++ app/src/main/java/com/sendbird/android/sample/main/BaseApplication.java
@@ -6,16 +6,38 @@ import android.app.Application;
 import com.sendbird.android.SendBird;
 import com.sendbird.android.sample.utils.PreferenceUtils;
 
+
+
 public class BaseApplication extends Application {
 
     private static final String APP_ID = "9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23"; // US-1 Demo
     public static final String VERSION = "3.0.40";
+    public static BaseApplication App		= null;
+    public static THLConfig Config	= null;
+    /** ================================================ */
 
+    public static BaseApplication getApp()
+    {
+        return App;
+    }
+    /** ================================================ */
     @Override
     public void onCreate() {
         super.onCreate();
         PreferenceUtils.init(getApplicationContext());
 
         SendBird.init(APP_ID, getApplicationContext());
+        App		= this;
+        Config	= new THLConfig(this);
+
+        Config.loadSettings();
+
+    }
+
+    public void onTerminate()
+    {
+        Config.saveSettings();
+
+        super.onTerminate();
     }
 }
